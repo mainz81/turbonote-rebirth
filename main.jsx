b72d02044898eed50ff15fcfffb7422066636ced
mainz81 <mainz81@gmail.com>
2025-06-30 23:47:37 -0400

    Create main.jsx
    Add main.jsx bootloader

---
+++ main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import TurboNote from './TurboNote';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<TurboNote />);
